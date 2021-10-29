2656aaff93833b5e1fb7bdbab3753e352ce4c2c4
zxlhhyccc <zxlhhy@gmail.com>
2021-10-30 00:07:24 +0800

    UPdate client.lua

--- luasrc/model/cbi/go-aliyundrive-webdav/client.lua
+++ luasrc/model/cbi/go-aliyundrive-webdav/client.lua
@@ -5,8 +5,10 @@
 local uci = luci.model.uci.cursor()
 local m, e
 
-m = Map("go-aliyundrive-webdav", translate("Go AliyunDrive WebDAV"),
-        translate("<a href=\"https://github.com/LinkLeong/go-aliyun-webdav\" target=\"_blank\">Project GitHub URL</a>"))
+m = Map("go-aliyundrive-webdav")
+m.title = translate("Go AliyunDrive WebDAV")
+
+m.description = translate(translate("<a href=\"https://github.com/LinkLeong/go-aliyun-webdav\" target=\"_blank\">Project GitHub URL</a>"))
 
 m:section(SimpleSection).template = "go-aliyundrive-webdav/go-aliyundrive-webdav_status"
 
@@ -16,15 +18,15 @@ e.anonymous = true
 enable = e:option(Flag, "enabled", translate("Enabled"))
 enable.rmempty = false
 
-rt_token = e:option(Value, "rt", translate("Refresh Token"),
-        translate("<a href=\"https://github.com/LinkLeong/go-aliyun-webdav#%E6%B5%8F%E8%A7%88%E5%99%A8%E8%8E%B7%E5%8F%96refreshtoken%E6%96%B9%E5%BC%8F\" target=\"_blank\">How to get refresh token</a>"))
+rt_token = e:option(Value, "rt", translate("Refresh Token"))
+rt_token.description = translate("<a href=\"https://github.com/LinkLeong/go-aliyun-webdav#%E6%B5%8F%E8%A7%88%E5%99%A8%E8%8E%B7%E5%8F%96refreshtoken%E6%96%B9%E5%BC%8F\" target=\"_blank\">How to get refresh token</a>")
 
 port = e:option(Value, "port", translate("Port"))
 port.default = "8085"
 port.datatype = "port"
 
-path = e:option(Value, "path", translate("Mounting directory"),
-       translate("Access aliyundrive directory, defaults /."))
+path = e:option(Value, "path", translate("Mounting directory"))
+path.description = translate("Access aliyundrive directory, defaults /.")
 path.default = "/"
 
 user = e:option(Value, "user", translate("Username"))
@@ -44,4 +46,6 @@ local e = luci.http.formvalue("cbi.apply")
 if e then
     io.popen("/etc/init.d/go-aliyundrive-webdav restart")
 end
+
+
 return m
